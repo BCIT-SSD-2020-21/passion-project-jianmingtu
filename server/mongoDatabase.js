799437e5e3e031b2be4f267d9ddcb771d02117ba
jianmingtu <tu_jm@yahoo.com>
2021-04-15 12:10:52 -0700

    test

--- server/mongoDatabase.js
+++ server/mongoDatabase.js
@@ -18,9 +18,22 @@ module.exports = async function() {
 
   async function getPosts(req) {
   const { limit = 100, skip = 0} = req 
-    const search =  req.query.search;
+    const search =  req.query?.search;
     const aggregateOptions = []
     
+    if (search) {
+      aggregateOptions.push({
+        $match: { 
+          $expr: {
+            $or: [ 
+              { $regexMatch: {input: '$pet_name', regex: new RegExp(`${search}`), options: "i" }},
+              { $regexMatch: {input: '$address_last_seen', regex: new RegExp(`${search}`), options: "i" }},
+              { $regexMatch: {input: '$user.username', regex: new RegExp(`${search}`), options: "i" }}
+            ]
+          } 
+        }
+      })
+    }
  
     return await db.collection('posts').aggregate(aggregateOptions).sort({ timestamp: -1, likes: 1}).skip(skip).limit(limit || 20).toArray()
   }
